93047e63fdc182dd56f42a3630a7a6376d84cfe9
AndriiLendiel <lendiel9333@gmail.com>
2024-02-12 20:20:06 +0200

    test

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,72 @@
-import contactsService from "../services/contactsServices.js";
+const contactsService = require ("../services/contactsServices.js");
 
-export const getAllContacts = (req, res) => {};
 
-export const getOneContact = (req, res) => {};
+const {HttpError} = require('../helpers')
+console.log(HttpError);
 
-export const deleteContact = (req, res) => {};
+const getAllContacts = async (req, res, next) => {
+    try {
+        const result = await contactsService.listContacts()
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+};
 
-export const createContact = (req, res) => {};
+const getOneContact = async (req, res, next) => {
+try {
+    const {id} =  req.params;
+    const result = await contactsService.getContactById(id)
+    if(!result) {
+        throw HttpError(404)
+    }
+    res.status(200).json(result)
+} catch (error) {
+    next(error)
+}
+};
 
-export const updateContact = (req, res) => {};
+const deleteContact = async (req, res, next) => {
+    const {id} = req.params
+    const result = await contactsService.removeContact(id)
+    if(!result) {
+        throw HttpError(404)
+    }
+    res.status(200).json({
+        message: 'Delete success'
+    })
+};
+
+const createContact = async (req, res, next) => {
+try {
+    const result = await contactsService.addContact(req.body)
+    res.status(201).json(result)
+} catch (error) {
+    next(error)
+}
+};
+
+const updateContact = async (req, res, next) => {
+try {
+    const {id} = req.params;
+    const result = await contactsService.updateById(id, req.body)
+    if(!result) {
+        throw HttpError(404)
+    }
+    res.status(404).json(result)
+} 
+catch (error) {
+    next(error)
+}
+};
+
+
+module.exports = {
+    getAllContacts,
+    getOneContact,
+    deleteContact,
+    createContact,
+    updateContact
+}
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,11 +1,15 @@
-import express from "express";
-import {
+const express = require ("express");
+const {
   getAllContacts,
   getOneContact,
   deleteContact,
   createContact,
   updateContact,
-} from "../controllers/contactsControllers.js";
+} = require ("../controllers/contactsControllers.js");
+const {validateBody} = require('../helpers/');
+
+const {createContactSchema} = require('../schemas/contactsSchemas.js')
+const {updateContactSchema} = require('../schemas/contactsSchemas.js')
 
 const contactsRouter = express.Router();
 
@@ -15,8 +19,8 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
 
-export default contactsRouter;
+module.exports = contactsRouter;
